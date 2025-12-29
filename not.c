874a1ca2c9918d529f5d8cde8e267a52da34af19
rohan7-debug <rohan.kumar2@rungta.org>
2025-12-29 15:29:48 +0530

    mini project

---
+++ not.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+int checkPassword(char password[])
+{
+    int i;
+    int hasDigit = 0;
+    int hasUpper = 0;
+
+    // Check minimum length
+    if (strlen(password) < 8)
+    {
+        return 0;
+    }
+
+    // Loop through each character
+    for (i = 0; password[i] != '\0'; i++)
+    {
+        // Check digit
+        if (password[i] >= '0' && password[i] <= '9')
+        {
+            hasDigit = 1;
+        }
+
+        // Check uppercase letter
+        if (password[i] >= 'A' && password[i] <= 'Z')
+        {
+            hasUpper = 1;
+        }
+    }
+    if (hasDigit == 1 && hasUpper == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    char password[50];
+    int result;
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    result = checkPassword(password);
+
+    if (result == 1)
+    {
+        printf("Password is STRONG\n");
+    }
+    else
+    {
+        printf("Password is WEAK\n");
+    }
+
+    return 0;
+}
